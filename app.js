ad2a00df17d40b4f0cfdc5e7e0e0a0e549ed0826
ali hasan <alihassanraja06@gmail.com>
2024-02-29 23:07:43 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,209 @@
+// practice questions
+// if else
+
+
+// var num1 =  prompt( "enter your marks") ;
+// // var num2 = 20
+// if(num1 > 100 ) {
+//     console.log("itne marks nhi aa sakte")
+// }
+// else if( num1 >= 33){
+//     console.log("pass");
+// }
+// else{
+//     console.log("shabash fail ho");
+// }
+
+
+
+
+
+
+// var num = 219;
+// prompt("enter your number");
+// if(num % 2 === 0){
+//     console.log("even")
+// }
+// else{
+//     console.log("odd")
+// }
+
+// question if else Q1
+
+// var num = +prompt("enter your number");
+// ;
+// if(num % 3 === 0){
+//     console.log("divisible by 3")
+// }
+// else{console.log("Number is not divisible by 3")}    
+
+
+
+// question if else Q2
+
+// var num = +prompt("enter your number");
+
+// if(num % 2 === 0){
+//     console.log("even")
+// }
+// else{
+//     console.log("odd")
+// }
+
+
+
+// question if else Q3
+
+// var num = +prompt("enter your age");
+// if(num > 18){
+//     console.log("old enough")
+// } 
+// else{
+//     console.log("Too young")
+// } 
+
+
+
+
+// question if else Q4
+
+// let username = prompt("enter your name");
+// let yourname = "ali hassan"
+// if (username === yourname) {
+// console.log("hello sir! hope you are doing okay")
+
+// }   
+
+
+
+// practice switch question
+
+// var day = prompt("what is the day");
+ 
+// switch (parseInt(day)){
+   
+//     case 1:
+//         console.log("today is monday ");
+//         break;
+
+
+//         case 2:
+//             console.log("today is tuesday ");
+//         break;
+
+
+//         case 3:
+//             console.log("today is wednesday");
+//         break;
+
+
+//         case 4:
+//             console.log("today is thursday");
+//         break;
+
+
+//         case 5:
+//             console.log("today is friday");
+//         break;
+
+
+//         case 6:
+//             console.log("today is saturday");
+//         break;
+
+
+//         case 7:
+//             console.log("today is sunday");
+//         break;
+
+    
+        
+//     default:
+//         console.log("enter valid week day");
+//         break;
+// }
+
+
+
+// question pdf Q5
+
+var numb = parseInt(prompt("enter your number"))
+
+switch(numb % 3) {
+    case 1:
+        console.log("number is divisible by 3")
+        break;
+        
+        case 2:
+        console.log("number is not divisible by 3")
+        break;
+
+    default:
+        console.log("Number is not divisible by 3");
+
+        break;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// question pdf Q6
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// question pdf Q7
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
